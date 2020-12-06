a6a47152717e64e2beeec5b6fdd9290e0d8c9b7f
Marmittfull <tiagomarmittt@gmail.com>
2020-12-06 12:45:51 -0300

    csv route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
 
+import cors from 'cors';
 import routes from './routes';
 import './database';
 import 'reflect-metadata';
@@ -10,6 +11,7 @@ import AppError from './errors/AppError';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
